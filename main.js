ae065c04fcbcaed79e3cf0f60195bf31d92708ed
Birkir Bjornsson <birkirbjornsson@desktop-vn2dk54.kea.local>
2018-06-08 14:47:33 +0200

    modal working 50%

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 let myObject;
 
 //getting data
-let json = FooBar.getData(); 
+let json = FooBar.getData();
 
 //parsing the json file into an object and returning string
 myObject = JSON.parse(json);
 
 
-function showQueue(){
+
+function showQueue() {
     //console.log(myObject.queue);
     document.querySelector("#queue text").textContent = myObject.queue.length;
 }
 
-function showPosters(){
+let postersButton = document.querySelector(".st8");
+
+function showPosters() {
     console.log(myObject.beertypes);
     document.querySelector("#post1 text").textContent = myObject.beertypes[1].name;
     document.querySelector("#post2 text").textContent = myObject.beertypes[2].name;
@@ -24,32 +27,44 @@ function showPosters(){
     document.querySelector("#post7 text").textContent = myObject.beertypes[7].name;
 }
 
+function hideModal() {
+    modal.classList.add('hide');
+}
+
 
 function showModalContent() {
-    let list = document.querySelector("#list");
-    let template = document.querySelector("#beerTemplate").content;
-    let modal = document.querySelector("#modal");
     
-    data.forEach(function (theProduct){
-    let name =  
+    let modal = document.querySelector("#modal");
+    let posters = ["#post1", "#post2", "#post3", "#post4", "#post5", "#post6", "#post7"];
 
-    //console.log(data);
-    modal.addEventListener('click', hideModal);
-	modal.querySelector(".name").textContent = myObject.beertypes.name;
-	modal.querySelector(".description").textContent = myObject.beertypes.description;
-	modal.querySelector(".alcohol").textContent = myObject.beertypes.alc;
-	modal.querySelector(".popularity").textContent = myObject.beertypes.popularity;
-	modal.querySelector(".pouringspeed").textContent = myObject.beertypes.pouringspeed;
-	//modal.querySelector('.image').src = data._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
-    modal.classList.remove('hide')
+    
+    posters.forEach(function (theBeer) {
+
+        let myBeer = myObject.beertypes[1];
+            postersButton.addEventListener('click', function () {
+			    showModalContent(theBeer);
+		});
+
+        modal.addEventListener('click', hideModal);
+
+        modal.querySelector(".name").textContent = myBeer.name;
+        modal.querySelector(".category").textContent = myBeer.category;
+        modal.querySelector(".appearance").textContent = "Appearance:  " + myBeer.description.appearance;
+        modal.querySelector(".aroma").textContent = "Aroma: " + myBeer.description.aroma;
+        modal.querySelector(".flavor").textContent = "Flavor: " + myBeer.description.flavor;
+        modal.querySelector(".mouthfeel").textContent ="Mouthfeel: " + myBeer.description.mouthfeel;
+        modal.querySelector(".overall").textContent ="Short Description: " + myBeer.description.overallImpression;
+        modal.querySelector(".alcohol").textContent = "Alcohol %: " + myBeer.alc;
+        modal.querySelector(".popularity").textContent ="Where do our customers rank this beer?: " + myBeer.popularity;
+        modal.querySelector(".pouringspeed").textContent ="How fast does it pour?: " + myBeer.pouringSpeed + " sec";
+        modal.querySelector('.image').src;
+        modal.classList.remove('hide')
+
     })
 };
 
-function hideModal() {
-	modal.classList.add('hide');
-}
+
 
 showQueue();
 showPosters();
 showModalContent();
-
